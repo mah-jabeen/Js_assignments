fff37ce30b097a9cf1af0f69baba287c72716115
Mahjabeen Akhtar <mahjabeenakhtar1214@gmail.com>
2024-10-14 00:26:13 +0500

    done chap1

---
+++ chap1_alert/index.js
@@ -0,0 +1,18 @@
+
+// ques1
+// alert("Welcome");
+
+// ques2
+// alert("Error! Please enter a valid password");
+
+// ques3
+// alert(`Welcome to JS Land...
+//     Happy coding!`);
+
+// ques4
+// alert("Welcome to JS Land..");
+// alert("Happy coding!");
+
+// ques5
+// console.log("Hello...I can run JS through my web browser's console")
+
