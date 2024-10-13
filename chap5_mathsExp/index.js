c37f57fca2514f0caf1aac49b5df2f413e3b04be
Mahjabeen Akhtar <mahjabeenakhtar1214@gmail.com>
2024-10-14 02:58:15 +0500

    till sum 5 multiplication of any number

---
+++ chap5_mathsExp/index.js
@@ -0,0 +1,70 @@
+// 1
+// var num1=2, num2=2,
+// total=num1+num2;
+// document.write("Sum of " + num1 + " and " + num2+ " is " + total);
+
+// 2
+// var num1=2, num2=2,
+// sub= num1-num2;
+// multi= num1*num2;
+// div= num1/num2;
+// mod= num1%num2;
+// document.write("Subtracion of " + num1 + " and " + num2+ " is " + sub);
+// document.write("<br> Multiplication of " + num1 + " and " + num2+ " is " + multi);
+// document.write("<br> Division of " + num1 + " and " + num2+ " is " + div);
+// document.write("<br>Modulus of " + num1 + " and " + num2+ " is " + mod);
+
+// 3
+// var num;
+// document.write("Value after variable declaration is: " + num);
+// num=5;
+// document.write("<br>Initial value: " + num);
+// num++;
+// document.write("<br>Value after increment is: " + num);
+// num=num+7;
+// document.write("<br>Value after addition is: " + num);
+// num--;
+// document.write("<br>Value after decrement is: " + num);
+// num=num%3;
+// document.write("<br>The remainder is: " + num);
+
+// 4
+// var ticket_price=600,quantity=5;
+// cost=600*quantity;
+// document.write("Total cost to buy " + quantity  + " tickets to a movie is " + cost + "PKR");
+
+// 5
+// var num, count=1 ;
+// num = prompt("enter any number");
+// document.write("Table of " + num);
+// multi= num*count;
+// document.write("<br>"+ num +" × 1 = " + multi);
+// count++;
+// multi= num*count;
+// document.write( "<br>"+ num +" × 2 = " + multi);
+// count++;
+// multi= num*count;
+// document.write("<br>"+num +" × 3 = " + multi);
+// count++;
+// multi= num*count;
+// document.write("<br>"+num +" × 4 = " + multi);
+// count++;
+// multi= num*count;
+// document.write("<br>"+num +" × 5 = " + multi);
+// count++;
+// multi= num*count;
+// document.write("<br>"+num +" × 6 = " + multi);
+// count++;
+// multi= num*count;
+// document.write("<br>"+num +" × 7 = " + multi);
+// count++;
+// multi= num*count;
+// document.write("<br>"+num +" × 8 = " + multi);
+// count++;
+// multi= num*count;
+// document.write("<br>"+num +" × 9 = " + multi);
+// count++;
+// multi= num*count;
+// document.write("<br>"+num +" × 10 = " + multi);
+
+// 6
