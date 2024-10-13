4219bf169b9e5551d0299a79a2f23716e46ab4ae
Mahjabeen Akhtar <mahjabeenakhtar1214@gmail.com>
2024-10-14 01:34:57 +0500

    var for numbers

---
+++ chap3_varForNum/index.js
@@ -0,0 +1,17 @@
+// 1
+// var age= 23;
+// alert("I am " + age + " years old");
+
+// 2
+// var track= 14;
+// alert("You have visited this site "+ track + " times");
+
+// 3
+// var birthYear = 2001;
+// document.write("My birth year is " + birthYear + "<br> Data type of my declared variable is number");
+
+// 4
+// var visitorName= "John Doe", 
+// productTitle="T-shirt" , 
+// quantity="5";
+// document.write(visitorName + " ordered " + quantity + " " +productTitle + "(s) on XYZ Clothing store");
