d85e3ca988c303c51c9929f4525b9cac56ca29e4
Mahjabeen Akhtar <mahjabeenakhtar1214@gmail.com>
2024-10-14 00:55:10 +0500

    chap 2 done

---
+++ chap2_varForStrings/index.js
@@ -0,0 +1,44 @@
+// 1
+// var username;
+
+// 2
+// var myName = "Mahjabeen Akhtar";
+// console.log(myName);
+
+// 3
+// var message = "Hello World";
+// alert(message);
+
+// 4
+// var name = "Johne Doe";
+// alert(name);
+
+// var age= "15 years old";
+// alert(age);
+
+// var msg="Certified Mobile Application Development";
+// alert(msg);
+
+
+// 5
+// var msg= `PIZZA
+// PIZZ
+// PIZ
+// PI
+// P`;
+// alert(msg);
+
+// 6
+// var email= "mahjabeen1214@gmail.com";
+// alert("My email address is " + email);
+
+// 7
+// var book= "A smarter way to learn Javascript";
+// alert("I am trying to learn from the Book " + book);
+
+// 8
+// document.write("Yah! I can write HTML content through Javascript");
+
+// 9
+// var design= "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(design);
