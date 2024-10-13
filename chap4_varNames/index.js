b33e1baa05223b9a685b66252a6286be940c3fb0
Mahjabeen Akhtar <mahjabeenakhtar1214@gmail.com>
2024-10-14 02:13:43 +0500

    var legal and illegal

---
+++ chap4_varNames/index.js
@@ -0,0 +1,20 @@
+// 1
+// var myName, myClass, myAge;
+
+// 2
+// legal variable
+// var myName, Myname$, _Name, my_Name, my1Name;
+// var my Name, 1myname , #9name, var, NAME*;
+
+// 3
+// document.write("<strong> Rules for naming JS variables</strong> <br> <br> <br> ");
+// var num= "numbers", 
+// sign= "$",
+// sign2= "_",
+// lett= "letters";
+// sen = "sensitive",
+// key = "keyword";
+// document.write("Variable names can only contain " + num + ", " + sign + " and " + sign2 +". For example: $my_1stVariable");
+// document.write("<br>Variables must begin with a " + lett + ", " + sign + " or " + sign2 + ". For example: $name, _name or name");
+// document.write("<br>Variable name are case " + sen);
+// document.write("<br>Variable names should not be JS " + key);
