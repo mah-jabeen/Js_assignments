a0bd24a0aa581126c1a81de41485c450e66e16ee
Mahjabeen Akhtar <mahjabeenakhtar1214@gmail.com>
2024-10-16 00:44:15 +0500

    maths expressions

--- chap5_mathsExp/index.js
+++ chap5_mathsExp/index.js
@@ -77,17 +77,90 @@
 // document.write("<br>" +fahrenheit + "oF is" + " "+ celsius + "oC");
 
 // 7
-var item1, item2, quantity1, quantity2, shipping;
-item1=prompt("Price of item 1: ");
-quantity1=prompt("Enter quantity of item1:");
-item2=prompt("Price of item 2: ");
-quantity2=prompt("Enter quantity of item2:");
-shipping=prompt("Enter shipping charges:");
-total= (item1*quantity1) + (item2*quantity2) + shipping;
-document.write("<strong> Shopping Cart </strong><br><br><br>");
-document.write("Price of item 1 is " + item1 + "<br>");
-document.write("Quantity of item 1 is " + quantity1 + "<br>");
-document.write("Price of item 2 is " + item2 + "<br>");
-document.write("Quantity of item 2 is " + quantity2 + "<br>");
-document.write("Shipping charges "+ shipping + "<br>");
-document.write("Total cost of your order is " + total + "<br>");
+// var  item1, item2, quantity1, quantity2, shipping;
+// item1=prompt("Price of item 1: ");
+// quantity1=prompt("Enter quantity of item1:");
+// item2=prompt("Price of item 2: ");
+// quantity2=prompt("Enter quantity of item2:");
+// shipping=Number(prompt("Enter shipping charges:"));
+// total= (item1*quantity1) + (item2*quantity2) + shipping;
+// document.write("<strong> Shopping Cart </strong><br><br><br>");
+// document.write("Price of item 1 is " + item1 + "<br>");
+// document.write("Quantity of item 1 is " + quantity1 + "<br>");
+// document.write("Price of item 2 is " + item2 + "<br>");
+// document.write("Quantity of item 2 is " + quantity2 + "<br>");
+// document.write("Shipping charges "+ shipping + "<br> <br>");
+// document.write("Total cost of your order is " + total + "<br>");
+
+
+// 8
+// var totalmarks, marksobt;
+// totalmarks= prompt("Enter totalmarks");
+// marksobt=prompt("Enter marks obtained");
+
+// document.write(" <strong> Marksheet </strong><br><br><br>");
+// document.write("Total marks: " + totalmarks );
+// document.write("<br> Marks obtained: " + marksobt  );
+// var percentage= (marksobt/totalmarks)*100;
+// document.write("<br> Percentage: " + percentage + "%");
+
+
+// 9
+// var pkr1,pkr2 , us_exchangerate=104.80, riyal_exgrate=28;
+// pkr1=10*us_exchangerate;
+// pkr2=25*riyal_exgrate;
+// total=pkr1+pkr2;
+// document.write("<strong>Currency in PKR </strong> <br><br>");
+// document.write("Total currency in PKR: " +total);
+
+
+// 10
+// var num=3;
+// total= ((num+5)*2)/2;
+// document.write("total " + total);
+
+
+// 11
+// var cur_year, birth_year;
+// birth_year= prompt("enter your birthyear");
+// cur_year=prompt("enter current year");
+// age= cur_year - birth_year;
+// document.write("Birth Year: " + birth_year);
+// document.write("<br>Current Year: " + cur_year);
+// document.write("<br>Your age is: " + age);
+
+
+// 12
+// var diameter;
+// diameter=prompt("enter diameter");
+// var radius,circumference, pi=3.142;
+// radius=diameter/2;
+// document.write("Radius of circle: " +radius)
+// circumference=2*pi*radius;
+// document.write("<br>The circumference is: "+circumference);
+// area= pi*(radius*radius);
+// document.write("<br>The area is: " +area);
+
+// 13
+// var fav_snack, curr_age, max_age, est_amount, total_eat;
+// fav_snack=prompt("enter your fav snack");
+// curr_age=prompt("Enter your current age");
+// max_age=prompt("Enter maximum age");
+// est_amount=prompt("enter estimated amount of snack you eat in one day");
+// total_eat=(max_age-curr_age)*est_amount;
+// document.write("<h1><Strong> The Lifetime supply calculator </strong></h1>");
+// document.write("<br>Favourite Snack: " + fav_snack);
+// document.write("<br>Current age: " + curr_age);
+// document.write("<br>Estimated Maximum Age: " +max_age);
+// document.write("<br>Amount of snack per day: "+est_amount);
+// document.write("<br>You will need " +total_eat +" " +fav_snack+"to last you until the ripe old age of " + max_age);
+
+
+
+
+
+
+
+
+
+
