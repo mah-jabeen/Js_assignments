f5502651e1c18aacb446da8245785c99c9ab7d95
Mahjabeen Akhtar <mahjabeenakhtar1214@gmail.com>
2024-10-16 02:24:55 +0500

    its conditions

---
+++ chap9-11condionalstatements/index.js
@@ -0,0 +1,92 @@
+// 1
+// var city;
+// city=prompt("enter any city");
+// if (city=== "karachi" ){
+//     document.write("Welcome to city of lights")
+// }else{
+//     document.write("Hello");
+// }
+
+// 2
+// var gender;
+// gender=prompt("enter your gender");
+// if(gender==="male"){
+//     document.write("Good Morning!! Sir");
+// }else if(gender==="female"){
+//     document.write("Good Morning!! Ma'am");
+// }else{
+//     document.write("error");
+// }
+
+// 3
+// var color;
+// color=prompt(`enter any color from below
+//     red,yellow,green`)
+// if(color==="red" ){
+//     document.write("Must stop");
+// }else if(color === "yellow"){
+//     document.write("Ready to move");
+// }else if(color === "green"){
+//     document.write("Move now");
+// }else{
+//     document.write("write correct color");
+// }
+
+// 4
+// var fuel;
+// fuel=prompt("enter fuel in your car in litres");
+// if(fuel<0.25){
+//     document.write("Please refill the fuel in your car");
+// }else{
+//     document.write("Your fuel is enough");
+// }
+
+// 5
+// a)
+// var a = 4; 
+// if (++a === 5){ 
+// alert("given condition for variable a is true"); 
+// } // this will show alert
+
+// b)
+//  var b = 82; 
+// if (b++ === 83){ 
+// alert("given condition for variable b is true"); 
+// } // this will not show alert
+
+// c)
+// var c = 12; 
+// if (c++ === 13){ 
+// alert("condition 1 is true"); 
+// } 
+// if (c === 13){ 
+// alert("condition 2 is true"); 
+// } // this will show alert
+// if (++c < 14){ 
+// alert("condition 3 is true"); 
+// } 
+// if(c === 14){ 
+// alert("condition 4 is true"); 
+// } // this will show alert
+
+// d)
+// var materialCost = 20000; 
+// var laborCost = 2000; 
+// var totalCost = materialCost + laborCost; 
+// if (totalCost === laborCost + materialCost){ 
+// alert("The cost equals"); 
+// } // will show alert
+
+// e)
+// if (true){ 
+//     alert("True"); 
+//     }  // this wil show alert
+//     if (false){ 
+//     alert("False"); 
+//     } 
+
+// f)
+// if("car" < "cat"){ 
+//     alert("car is smaller than cat"); 
+//     } // will show alert
+
