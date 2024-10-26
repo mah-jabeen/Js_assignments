3c5ce9b9e39020b141fa8978891ddf3b5ddea0d2
Mahjabeen Akhtar <mahjabeenakhtar1214@gmail.com>
2024-10-26 20:37:17 +0500

    slice, splice, pop and push

---
+++ chap14-16Arrays/index.js
@@ -0,0 +1,70 @@
+// 1
+// var studentNames = [];
+
+// 2
+// var studentNamesObject = new Array();
+
+// 3
+// var stringArray = ["apple", "banana", "cherry"];
+
+// 4
+// var numbersArray = [1, 2, 3, 4, 5];
+
+// 5
+// var booleanArray = [true, false, true];
+
+// 6
+// var mixedArray = ["text", 42, true, { key: "value" }, [1, 2, 3]];
+
+// 7
+// var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil.", "PhD"];
+
+// document.write("<h3>Qualifications in Pakistan:</h3>");
+// for (var i = 0; i < qualifications.length; i++) {
+//   document.write((i + 1) + ") " + qualifications[i] + "<br>");
+// }
+
+// 8
+// var studentNames = ["Michael", "John", "Tony"];
+// var studentScores = [320, 230, 480];
+// var totalMarks = 500;
+
+// for (var i = 0; i < studentNames.length; i++) {
+//   var percentage = (studentScores[i] / totalMarks) * 100;
+//   document.write("Score of " + studentNames[i] + " is " + studentScores[i] + ". Percentage: " + percentage + "%<br>");
+// }
+
+// 9
+// var colors = ["Red", "Green", "Blue"];
+// document.write("Initial Colors: " + colors + "<br>");
+
+// var colorToAddAtBeginning = prompt("Enter a color to add at the beginning:");
+// colors.unshift(colorToAddAtBeginning);
+// document.write("After adding color at the beginning: " + colors + "<br>");
+
+// var colorToAddAtEnd = prompt("Enter a color to add at the end:");
+// colors.push(colorToAddAtEnd);
+// document.write("After adding color at the end: " + colors + "<br>");
+
+// colors.unshift("Purple", "Yellow");
+// document.write("After adding two more colors at the beginning: " + colors+ "<br>");
+
+// colors.shift();
+// document.write("After deleting the first color: " + colors + "<br>");
+
+// colors.pop();
+// document.write("After deleting the last color: " + colors+ "<br>");
+
+// var indexToAddColor = parseInt(prompt("Enter the index at which you want to add a color:"));
+// var colorToAdd = prompt("Enter the color name:");
+// colors.splice(indexToAddColor, 0, colorToAdd);
+// document.write("After adding color at index " + indexToAddColor + ": " + colors+ "<br>");
+
+// var indexToDeleteColor = parseInt(prompt("Enter the index from which you want to delete color(s):"));
+// var numberOfColorsToDelete = parseInt(prompt("Enter the number of colors to delete:"));
+// colors.splice(indexToDeleteColor, numberOfColorsToDelete);
+// document.write("After deleting " + numberOfColorsToDelete + " color(s) from index " + indexToDeleteColor + ": " + colors + "<br>");
+
+
+// 10
+
