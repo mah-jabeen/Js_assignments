e524c21868ee012ebfe8b6634558d9acbaa7a2b4
Mahjabeen Akhtar <mahjabeenakhtar1214@gmail.com>
2024-10-30 23:28:52 +0500

    string methods

---
+++ chap21-25StringMethods/app.js
@@ -0,0 +1,78 @@
+// 1
+// var firstname= prompt("enter your first name:");
+// var lastname= prompt("enter your last name:");
+// var fullname;
+// fullname= firstname+lastname;
+// alert("Welcome " + fullname);
+
+// 2
+// var fave= prompt("enter your favourite mobile phone model");
+// document.write("My favourite phone is: " + fave +"<br>");
+// lengthy= fave.length;
+// document.write("Length of string "+ lengthy);
+
+// 3
+// var letter="Pakistani";
+// document.write("String: " + letter );
+// mystr= letter.indexOf("n");
+// document.write(" <br>Index of 'n': " + mystr );
+
+// 4
+// var letter="Hello World";
+// document.write("String: " + letter );
+// mystr= letter.lastIndexOf("l");
+// document.write(" <br>Last Index of 'l': " + mystr );
+
+// 5
+// var letter="Pakistani";
+// document.write("String: " + letter );
+// mystr= letter.charAt(3);
+// document.write("<br>Character at index 3: " + mystr );
+
+// 6
+// var firstName = "mahjabeen";
+// var lastName = "akhtar";
+// var fullName = firstName + " " + lastName;
+// console.log(fullName); 
+
+// 7
+// var city="Hyderabad";
+// document.write("City: " + city );
+// city= city.replace("Hyderabad","Islamabad");
+// document.write("<br>After replacement: " + city );
+
+// 8
+// var message = "Ali and Sami are best friends. They play cricket and football together."; 
+// message= message.replace(/and/g,"&");
+// document.write(message);
+
+// 9
+// var value="472";
+// document.write("Value: " +value);
+// document.write("<br>Type: " + typeof(value));
+// value2= Number(value);
+// document.write("<br>Value: " + value2);
+// document.write("<br>Type: " + typeof(value2));
+
+// 10
+// userinput= prompt("Enter a word");
+// document.write("User input: " +userinput);
+// document.write("<br>Upper case: " +userinput.toUpperCase());
+
+// 11
+// var str = "hello world, how are you?";
+// var words = str.split(' ');
+// for (var i = 0; i < words.length; i++) {
+//   words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();   
+
+// }
+// var titleCaseStr = words.join(' ');
+// console.log(titleCaseStr); 
+
+
+//12
+var num=35.36;
+document.write("Number: " + num);
+result= parseFloat(num);
+document.write("<br>Result: " + result);
+
